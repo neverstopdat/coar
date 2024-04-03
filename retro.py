01c9bd783a3f2d8251ff05f76f9bad35d9cee85e
neverstopdat <dadatnim@hotmail.com>
2024-04-03 11:19:32 +0900

    Update retro.py

--- retro.py
+++ retro.py
@@ -25,7 +25,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo-instruct"
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 if "messages" not in st.session_state:
  st.session_state.messages = []
 
